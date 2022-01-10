cf15690e266f784b368404a11c4e6bbea3864b54
zacharyholland <90477622+zacharyholland@users.noreply.github.com>
2022-01-10 08:57:17 -0700

    Initial check-in of module FirstASPNetWebApp

---
+++ FirstASPNetWebApp/wwwroot/js/gradecalc.js
@@ -0,0 +1,12 @@
+﻿var finalGrade = 0;
+
+//Form calculation
+$("#btnCalc").click(function () {
+    finalGrade =
+        (parseInt($("#assignments").val()) * .55) +
+        (parseInt($("#groupprojects").val()) * .05) +
+        (parseInt($("#quizzes").val()) * .1) +
+        (parseInt($("#exams").val()) * .2) +
+        (parseInt($("#intex").val()) * .1);
+
+    alert("Your final grade is " + String(finalGrade));
